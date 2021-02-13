574942a195e7b25d1096ac22cb5e052b7798d458
jplck <pollack.jan@gmail.com>
2021-02-13 20:23:45 +0100

    added test python container flask app

--- test-assets/docker/src/app.py
+++ test-assets/docker/src/app.py
@@ -1,9 +1,27 @@
+from multiprocessing import Process
 from flask import Flask
+from flask import request
+import time
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World'
+@app.route('/api', methods=['POST'])
+def request_api():
+   content = request.json
+   callbackUrl = content['externalTriggerCallbackUrl']
+   p1 = Process(target=doing_work, args=(callbackUrl,))
+   p1.start()
+   return 'OK'
+
+def doing_work(callbackUrl):
+   print('working...')
+   print(callbackUrl)
+   time.sleep(5)
+   res = requests.post(callbackUrl, json={'blobUri': 'jim hopper', 'success': True})
+   print(res)
+   print('work done...')
+
 
 if __name__ == '__main__':
    app.run(host ='0.0.0.0', port = 5001, debug = True)
