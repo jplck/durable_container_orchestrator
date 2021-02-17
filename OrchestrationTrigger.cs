e2096f8d0d1cc44d079b0e065ba2ec3871764e5e
jplck <pollack.jan@gmail.com>
2021-02-17 14:06:40 +0100

    added orchestratiuon termination to reset trigger functiob

--- OrchestrationTrigger.cs
+++ OrchestrationTrigger.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace ContainerRunnerFuncApp
 {
@@ -28,7 +29,6 @@ namespace ContainerRunnerFuncApp
             };
 
             string instanceId = await starter.StartNewAsync("ACI_Main_Orchestrator_Func", events);
-            
             log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
             return starter.CreateCheckStatusResponse(req, instanceId);
@@ -36,13 +36,24 @@ namespace ContainerRunnerFuncApp
 
         [FunctionName("Orchestration_Reset_Trigger_Func_HTTP")]
         public static async Task<IActionResult> TriggerResetRun(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestMessage req,
-            [DurableClient] IDurableEntityClient entityClient,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "reset/{instanceId}")] HttpRequestMessage req,
+            [DurableClient] IDurableEntityClient entityClient, [DurableClient] IDurableOrchestrationClient durableClient,
+            string instanceId,
             ILogger log)
         {
-            log.LogInformation("Start entity reset.");
-            var entityId = new EntityId("ContainerInstanceStatusEntity", "ContainerInstanceStatusEntity");
-            await entityClient.SignalEntityAsync(entityId, "Reset");
+            try
+            {
+                log.LogInformation($"Stop orchestrator with instanceId: {instanceId}");
+                await durableClient.TerminateAsync(instanceId, "Due to reset");
+
+                log.LogInformation("Start entity reset.");
+                var entityId = new EntityId("ContainerInstanceStatusEntity", "ContainerInstanceStatusEntity");
+                await entityClient.SignalEntityAsync(entityId, "Reset");
+            }
+            catch (Exception)
+            {
+                return new BadRequestResult();
+            }
             return new AcceptedResult();
         }
     }
